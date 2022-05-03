d26d4fd86d7de7e27632b19e2086e4f2a87565c4
kevin alexsander <kevin13348@gmail.com>
2022-05-03 08:58:11 -0300

    created a login module

--- TESTE BARCODE/Form1.cs
+++ TESTE BARCODE/Form1.cs
@@ -46,63 +46,11 @@ namespace TESTE_BARCODE
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-
         }
 
         private void btLogar_Click(object sender, EventArgs e)
         {
-            string strcon = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\10.33.33.2\publico\SERVICE.accdb";
-            string comando = "Select * from NOME where SENHA =@SENHA";
-            OleDbConnection con = new OleDbConnection(strcon);
-            OleDbCommand com = new OleDbCommand(comando, con);
-
-            com.Parameters.AddWithValue("Senha", tbSenha.Text);
-            com.CommandType = CommandType.Text;
-            OleDbDataReader reader;
-            con.Open();
-            try
-            {
-                reader = com.ExecuteReader();
-                if (reader.Read())
-                {
-                    tbNome.Text = reader[0].ToString();
-                    lbSenha.Visible = false;
-                    tbSenha.Visible = false;
-                    btLogar.Visible = false;
-
-                    rbAprovado.Enabled = false;
-                    rbReprovado.Enabled = false;
-
-                    imgLogoLenovo.Visible = true;
-
-                    btMostrarAprovados.Enabled = true;
-                    btMostrarReprovados.Enabled = true;
-                    btOcultar.Enabled = true;
-
-                    tbSN.Enabled = true;
-
-                    btLogout.Visible = true;
-
-                    rbAprovado.Checked = false;
-                    rbReprovado.Checked = false;
-
-                    tbSN.Focus();
-
-                    con.Close();
-                }
-                else
-                    MessageBox.Show("Senha incorreta! Usuário não encontrado");
-                tbSenha.Clear();
-                tbSenha.Focus();
-
-            }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show("Erro: " + ex.ToString());
-            }
-            con.Close();
+            logar();
         }
 
         private void tbSenha_KeyDown(object sender, KeyEventArgs e)
@@ -192,9 +140,6 @@ namespace TESTE_BARCODE
                     dGridAprovados.Columns[37].Visible = false;
                     dGridAprovados.Columns[38].Visible = false;
                 }
-
-
-
                 con.Close();
 
             }
---
+++ TESTE BARCODE/modulos/login/logar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace TESTE_BARCODE {
+    partial class Form1 {
+        private void logar() {
+            string strcon = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\10.33.33.2\publico\SERVICE.accdb";
+            string comando = "Select * from NOME where SENHA =@SENHA";
+            OleDbConnection con = new OleDbConnection(strcon);
+            OleDbCommand com = new OleDbCommand(comando, con);
+
+            com.Parameters.AddWithValue("Senha", tbSenha.Text);
+            com.CommandType = CommandType.Text;
+            OleDbDataReader reader;
+            con.Open();
+            try
+            {
+                reader = com.ExecuteReader();
+                if (reader.Read())
+                {
+                    tbNome.Text = reader[0].ToString();
+                    lbSenha.Visible = false;
+                    tbSenha.Visible = false;
+                    btLogar.Visible = false;
+
+                    rbAprovado.Enabled = false;
+                    rbReprovado.Enabled = false;
+
+                    imgLogoLenovo.Visible = true;
+
+                    btMostrarAprovados.Enabled = true;
+                    btMostrarReprovados.Enabled = true;
+                    btOcultar.Enabled = true;
+
+                    tbSN.Enabled = true;
+
+                    btLogout.Visible = true;
+
+                    rbAprovado.Checked = false;
+                    rbReprovado.Checked = false;
+
+                    tbSN.Focus();
+
+                    con.Close();
+                }
+                else
+                    MessageBox.Show("Senha incorreta! Usuário não encontrado");
+                tbSenha.Clear();
+                tbSenha.Focus();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            con.Close();
+        }
+    }
+}
